2c9b1f1d2fb6f03e09bd8ff8a60d40476ec825a7
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-10-02 18:05:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+
 import Navbar from './components/Navbar'
 import Work from './components/Work'
 import Stripes from './components/Stripes'
 import Products from './components/Products'
-import Marquees from './components/marquees'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
 import LocomotiveScroll from 'locomotive-scroll';
